aed599ec7e90fff981431c852ad87723d454dd7f
usscentroinformatica <usscentroinformatica@gmail.com>
2026-01-29 10:11:37 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  build: {
+    outDir: 'dist',  // Asegúrate que sea 'dist'
+  },
 })
